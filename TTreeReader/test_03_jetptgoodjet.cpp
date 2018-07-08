4adc6192f6f4676f72a37fe389be45bf5f131a25
Gordon Watts <gordon@gordonandpaula.net>
2018-07-08 16:49:04 +0300

    Decl change offers substantial speedup

--- TTreeReader/test_03_jetptgoodjet.cpp
+++ TTreeReader/test_03_jetptgoodjet.cpp
@@ -5,16 +5,20 @@ void test_03_jetptgoodjet()
 
     auto file = TFile::Open("/mnt/c/Users/gordo/Documents/GRIDDS/user.emma.mc15_13TeV.361023.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ3W.merge.DAOD_EXOT15.e3668_s2576_s2132_r7773_r7676_p2952.v201/copied/ntuples_QCD_JZ3__0_addFullEtaMLP.root", "READ");
     TTreeReader reader("recoTree", file);
-    TTreeReaderArray<double> jet_pts(reader, "CalibJet_pT");
-    TTreeReaderArray<double> jet_eta(reader, "CalibJet_eta");
+    //TTreeReaderArray<double> jet_pts(reader, "CalibJet_pT");
+    TTreeReaderValue<vector<double>> jet_pts(reader, "CalibJet_pT");
+    //TTreeReaderArray<double> jet_eta(reader, "CalibJet_eta");
+    TTreeReaderValue<vector<double>> jet_eta(reader, "CalibJet_eta");
 
     auto f = new TFile("../03-reader.root", "RECREATE");
     auto h = new TH1F("jetpt", "Jet pT; Jet pT [GeV]", 100, 0.0, 500.0);
 
     while(reader.Next()) {
-        for (int i = 0; i < jet_pts.GetSize(); i++){
-            if (abs(jet_eta[i]) < 1.0) {
-                h->Fill(jet_pts[i]);
+        auto pts (*jet_pts);
+        auto eta (*jet_eta);
+        for (int i = 0; i < pts.size(); i++){
+            if (abs(eta[i]) < 1.0) {
+                h->Fill(pts[i]);
             }
         }
     }
--- TTreeReader/test_04_metgoodjet.cpp
+++ TTreeReader/test_04_metgoodjet.cpp
@@ -5,8 +5,8 @@ void test_04_metgoodjet()
 
     auto file = TFile::Open("/mnt/c/Users/gordo/Documents/GRIDDS/user.emma.mc15_13TeV.361023.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ3W.merge.DAOD_EXOT15.e3668_s2576_s2132_r7773_r7676_p2952.v201/copied/ntuples_QCD_JZ3__0_addFullEtaMLP.root", "READ");
     TTreeReader reader("recoTree", file);
-    TTreeReaderArray<double> jet_pts(reader, "CalibJet_pT");
-    TTreeReaderArray<double> jet_eta(reader, "CalibJet_eta");
+    TTreeReaderValue<vector<double>> jet_pts(reader, "CalibJet_pT");
+    TTreeReaderValue<vector<double>> jet_eta(reader, "CalibJet_eta");
     TTreeReaderValue<double> a(reader, "event_HTMiss");
 
     auto f = new TFile("../04-reader.root", "RECREATE");
@@ -14,8 +14,10 @@ void test_04_metgoodjet()
 
     while(reader.Next()) {
         int counter = 0;
-        for (int i = 0; i < jet_pts.GetSize(); i++){
-            if (abs(jet_eta[i]) < 1.0 && jet_pts[i] > 40.0) {
+        auto etas(*jet_eta);
+        auto pts(*jet_pts);
+        for (int i = 0; i < pts.size(); i++){
+            if (abs(etas[i]) < 1.0 && pts[i] > 40.0) {
                 counter++;
             }
         }
