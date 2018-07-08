1dcdc620ca1996881bfcd3e4288038e13dcbc0d6
Gordon Watts <gordon@gordonandpaula.net>
2018-07-08 15:31:08 +0300

    Do a jet cut test

--- dataframe/test_01_met.cpp
+++ dataframe/test_01_met.cpp
@@ -9,7 +9,7 @@ void test_01_jetpt()
 {
     TStopwatch sw;
     sw.Start();
-    ROOT::EnableImplicitMT();
+    //ROOT::EnableImplicitMT();
     ROOT::RDataFrame df("recoTree", "/mnt/c/Users/gordo/Documents/GRIDDS/user.emma.mc15_13TeV.361023.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ3W.merge.DAOD_EXOT15.e3668_s2576_s2132_r7773_r7676_p2952.v201/copied/ntuples_QCD_JZ3__0_addFullEtaMLP.root");
 
     auto f = new TFile("../01-dataframe.root", "RECREATE");
--- dataframe/test_03_jetptwithetacut.cpp
+++ dataframe/test_03_jetptwithetacut.cpp
@@ -1,20 +1,25 @@
-// Test 01 - Plot jet pT
+// Test 01 - Plot jet pT with an eat cut
 
 // Run with the command:
 //
 // Testing done with root 6.14
 //
 
-void test_02_jetpt()
+void test_03_jetptwithetacut()
 {
     TStopwatch sw;
     sw.Start();
     //ROOT::EnableImplicitMT();
     ROOT::RDataFrame df("recoTree", "/mnt/c/Users/gordo/Documents/GRIDDS/user.emma.mc15_13TeV.361023.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ3W.merge.DAOD_EXOT15.e3668_s2576_s2132_r7773_r7676_p2952.v201/copied/ntuples_QCD_JZ3__0_addFullEtaMLP.root");
 
-    auto f = new TFile("../02-dataframe.root", "RECREATE");
-    auto met = df.Histo1D("CalibJet_pT");
-    met->Write();
+    auto f = new TFile("../03-dataframe.root", "RECREATE");
+
+    // Define a good jet & plot it
+    auto df_good = df.Define("goodjet", "abs(CalibJet_eta) < 1.0")
+           .Define("goodjet_pt", "CalibJet_pT[goodjet]");
+    auto jetpt = df_good.Histo1D("goodjet_pt");
+
+    jetpt->Write();
     f->Write();
     f->Close();
     delete f;
