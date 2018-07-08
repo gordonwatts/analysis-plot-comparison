f6445a1aaf334f67c676a2057eaf86931f8df6d4
Gordon Watts <gordon@gordonandpaula.net>
2018-07-08 14:48:30 +0300

    Second test with jet pT

---
+++ dataframe/test_01_met.cpp
@@ -0,0 +1,24 @@
+// Test 01 - Plot jet pT
+
+// Run with the command:
+//  time root -b -l -q test_01_jetpt.cpp
+// Testing done with root 6.14
+//
+
+void test_01_jetpt()
+{
+    TStopwatch sw;
+    sw.Start();
+    ROOT::EnableImplicitMT();
+    ROOT::RDataFrame df("recoTree", "/mnt/c/Users/gordo/Documents/GRIDDS/user.emma.mc15_13TeV.361023.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ3W.merge.DAOD_EXOT15.e3668_s2576_s2132_r7773_r7676_p2952.v201/copied/ntuples_QCD_JZ3__0_addFullEtaMLP.root");
+
+    auto f = new TFile("../01-dataframe.root", "RECREATE");
+    auto met = df.Histo1D("event_HTMiss");
+    met->Write();
+    f->Write();
+    f->Close();
+    delete f;
+
+    sw.Stop();
+    sw.Print();
+}
--- dataframe/test_02_jetpt.cpp
+++ dataframe/test_02_jetpt.cpp
@@ -5,15 +5,15 @@
 // Testing done with root 6.14
 //
 
-void test_01_jetpt()
+void test_02_jetpt()
 {
     TStopwatch sw;
     sw.Start();
     ROOT::EnableImplicitMT();
     ROOT::RDataFrame df("recoTree", "/mnt/c/Users/gordo/Documents/GRIDDS/user.emma.mc15_13TeV.361023.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ3W.merge.DAOD_EXOT15.e3668_s2576_s2132_r7773_r7676_p2952.v201/copied/ntuples_QCD_JZ3__0_addFullEtaMLP.root");
 
-    auto f = new TFile("../01-dataframe.root", "RECREATE");
-    auto met = df.Histo1D("event_HTMiss");
+    auto f = new TFile("../02-dataframe.root", "RECREATE");
+    auto met = df.Histo1D("CalibJet_pT");
     met->Write();
     f->Write();
     f->Close();
